cb2ff354877e5c54f804960fc857fc5f768174da
Alequiso <Alexsandro.leite@sptech.school>
2023-05-30 20:49:29 -0300

    jfiejifji

--- mavenproject1/src/main/java/RunShellScript/RunShellScript.java
+++ mavenproject1/src/main/java/RunShellScript/RunShellScript.java
@@ -1,11 +1,11 @@
 
 package RunShellScript;
-
+import java.io.IOException;
     import java.io.*;
 
 public class RunShellScript {
     public static void main(String[] args) throws IOException {
-        String[] cmd = { "/bin/sh", "/root/Desktop/JavaTOOD/executavel-com-interface/reiniciar.sh" };
+        String[] cmd = { "/bin/sh", "-c", "sudo reboot" };
         Process p = Runtime.getRuntime().exec(cmd);
         BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
         String line = "";
